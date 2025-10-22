99d3ebcd8b4a97ba18107acc2496f6df4f51f1f8
Amirzhan Albariyev <albariev123@gmail.com>
2025-10-23 07:24:24 +0800

    added log to server

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
-import argparse
 import textwrap
 import logging
 import requests
 import telegram
-import time
 import os
-
 from dotenv import load_dotenv
 from time import sleep
-
 
 URL = 'https://dvmn.org/api/long_polling/'
 
@@ -21,64 +17,74 @@ class TelegramLogsHandler(logging.Handler):
 
     def emit(self, record):
         log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+        try:
+            self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+        except Exception:
+            pass
 
 
 def main():
     load_dotenv()
     devman_token = os.environ['DEVMAN_TOKEN']
     bot_token = os.environ['TG_BOT_TOKEN']
-    headers = {'Authorization': f'Token {devman_token}'}
     chat_id = os.environ['CHAT_ID']
+
     bot = telegram.Bot(token=bot_token)
-    bot.logger.addHandler(TelegramLogsHandler(bot, chat_id))
-    bot.logger.warning('Бот запущен')
+
+    logger = logging.getLogger('DevmanBot')
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler('/opt/devman/DevmanCheckBot/bot.log')
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+
+    tg_handler = TelegramLogsHandler(bot, chat_id)
+    tg_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(tg_handler)
+
+    logger.info('Бот запущен успешно.')
+
+    headers = {'Authorization': f'Token {devman_token}'}
     timestamp = ''
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
+
     while True:
         try:
-            response = requests.get(URL,
-                                    headers=headers,
-                                    params={"timestamp": timestamp},
-                                    timeout=90,)
+            response = requests.get(URL, headers=headers, params={"timestamp": timestamp}, timeout=90)
             response.raise_for_status()
             check_results = response.json()
+
             if check_results["status"] == "timeout":
                 timestamp = check_results["timestamp_to_request"]
-            else:
-                timestamp = check_results["new_attempts"][0]["timestamp"]
-                is_negative = check_results["new_attempts"][0]["is_negative"]
-                lesson_title = (
-                    check_results["new_attempts"][0]["lesson_title"]
-                )
-                lesson_url = check_results["new_attempts"][0]["lesson_url"]
+                continue
+
+            attempt = check_results["new_attempts"][0]
+            timestamp = attempt["timestamp"]
+            lesson_title = attempt["lesson_title"]
+            lesson_url = attempt["lesson_url"]
+            is_negative = attempt["is_negative"]
 
             if is_negative:
-                bot.send_message(
-                    text=textwrap.dedent(f'''
-                    У вас проверили работу «{lesson_title}»\n
-                    К сожалению в работе нашлись ошибки.
-                    Ссылка на урок:{lesson_url}
-                    '''),
-                    chat_id=chat_id,
-                    )
+                text = textwrap.dedent(f"""
+                    У вас проверили работу «{lesson_title}» 😞
+                    К сожалению, в работе нашлись ошибки.
+                    Ссылка на урок: {lesson_url}
+                """)
             else:
-                bot.send_message(
-                    text=textwrap.dedent(f'''
-                    У вас проверили работу «{lesson_title}»\n
-                    Преподаватель одобрил работу, можно приступать
-                    к следующему уроку! Ссылка на урок:{lesson_url}
-                    '''),
-                    chat_id=chat_id,
-                    )
+                text = textwrap.dedent(f"""
+                    У вас проверили работу «{lesson_title}» 🎉
+                    Преподаватель одобрил работу! Можно приступать к следующему уроку.
+                    Ссылка на урок: {lesson_url}
+                """)
+
+            bot.send_message(chat_id=chat_id, text=text.strip())
+
         except requests.exceptions.ConnectionError:
+            logger.warning('Проблема с соединением. Переподключение через 10 секунд...')
             sleep(10)
         except requests.exceptions.ReadTimeout:
             pass
         except Exception as err:
-            error = f'Бот упал с ошибкой {str(err)}'
-            bot.logger.warning(error)
+            logger.exception(f'Бот упал с ошибкой: {err}')
 
 
 if __name__ == "__main__":
